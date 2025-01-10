7425f5298d0c83d39db32a118b68d88de5758236
AminKashiri <kashiri.amin@gmail.com>
2025-01-09 20:58:34 -0500

    Convert STL formula to optimization problem

--- STL.py
+++ STL.py
@@ -1,14 +1,19 @@
-from dataclasses import dataclass
-from typing import List
 import numpy as np
-from numbers import Number
+from typing import List
 import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import gurobipy as gp
+
+
+aux_name = lambda i: f"stl_aux{i}"
+const_name = lambda i: f"stl_const{i}"
 
 
 @dataclass
 class Operator:
     """
     A class that keeps the information for an operator, such as eventually (F), globally (G), and (&), or (|), negation (N) ...
+    It can also keep a predicate, for which name == "predicate".
     """
 
     name: str
@@ -26,6 +31,100 @@ class Operator:
             return f"{self.args[0]} {self.args[1]} {self.args[2]}"
         else:
             return "(" + f" {self.name} ".join(map(lambda x: str(x), self.args)) + ")"
+
+    def create_aux(self, opt, start_step, info, used_predicates):
+        self.opt = opt
+        if self.name == "F":
+            zs = []
+            steps = int(self.end / info["dt"])
+            for t in range(steps + 1):
+                z = self.args[0].create_aux(opt, start_step + t, info, used_predicates)
+                zs.append(z)
+
+            return self.disjunct_zs(zs, info)
+        elif self.name == "G":
+            zs = []
+            steps = int(self.end / info["dt"])
+            for t in range(steps + 1):
+                z = self.args[0].create_aux(opt, start_step + t, info, used_predicates)
+                zs.append(z)
+
+            return self.conjunct_zs(zs, info)
+        elif self.name == "N":
+            z = self.args[0].create_aux(opt, start_step, info, used_predicates)
+            nz = self.opt.addVar(vtype=gp.GRB.BINARY, name=aux_name(info["aux_count"]))
+            info["aux_count"] += 1
+            self.opt.addConstr(nz == 1 - z, const_name(info["const_count"]))
+            info["const_count"] += 1
+            return nz
+        elif self.name == "&":
+            zs = [self.args[0], self.args[1]]
+            return self.conjunct_zs(zs, info)
+        elif self.name == "|":
+            zs = [self.args[0], self.args[1]]
+            return self.disjunct_zs(zs, info)
+        elif self.name == "predicate":
+
+            z = used_predicates.get(str(self) + f"_{start_step}")
+            if z:
+                return z
+
+            #! Warn: get or create is only for test.
+            from utils import get_or_create_var
+            var = get_or_create_var(self.opt, self.args[0] + f"_{start_step}")
+
+            # var = self.opt.getVarByName(self.args[0] + f"_{start_step}")
+
+            # mu positive == satsify
+            if self.args[1] == ">":
+                mu = var - self.args[2]
+            else:
+                mu = -1 * (var - self.args[2])
+
+            z = self.opt.addVar(vtype=gp.GRB.BINARY, name=aux_name(info["aux_count"]))
+            info["aux_count"] += 1
+
+            self.opt.addConstr(mu <= M * z - et, const_name(info["const_count"]))
+            info["const_count"] += 1
+            self.opt.addConstr(
+                -1 * (mu) <= M * (1 - z) - et, const_name(info["const_count"])
+            )
+            info["const_count"] += 1
+
+            used_predicates[str(self) + f"_{start_step}"] = z
+            return z
+        else:
+            raise NotImplementedError("Operator not implemented")
+
+    def conjunct_zs(self, zs, info):
+        z_conj = self.opt.addVar(vtype=gp.GRB.BINARY, name=aux_name(info["aux_count"]))
+        info["aux_count"] += 1
+        sum_zs = 0
+        for z in zs:
+            self.opt.addConstr(z_conj <= z, const_name(info["const_count"]))
+            info["const_count"] += 1
+            sum_zs = sum_zs + z
+
+        self.opt.addConstr(
+            z_conj >= 1 - len(zs) + sum_zs, const_name(info["const_count"])
+        )
+        info["const_count"] += 1
+
+        return z_conj
+
+    def disjunct_zs(self, zs, info):
+        z_disj = self.opt.addVar(vtype=gp.GRB.BINARY, name=aux_name(info["aux_count"]))
+        info["aux_count"] += 1
+        sum_zs = 0
+        for z in zs:
+            self.opt.addConstr(z_disj >= z, const_name(info["const_count"]))
+            info["const_count"] += 1
+            sum_zs = sum_zs + z
+
+        self.opt.addConstr(z_disj <= sum_zs, const_name(info["const_count"]))
+        info["const_count"] += 1
+
+        return z_disj
 
 
 class STLParser:
@@ -165,29 +264,42 @@ class STLParser:
 
 
 if __name__ == "__main__":
-    STEP = 0.1
-    SIGNAL_START = 0
-    SIGNAL_END = 10
+    DT = 0.5
 
     # The formula for the signal
-    signal = lambda t: np.sin(t) + 0.5 * np.cos(2 * t)
+    # signal = lambda t: np.sin(t) + 0.5 * np.cos(2 * t)
 
+    spec = "F[1,2](G[0,1](x1 > 3))"
     # spec = "G[0,10](x > 1.2)"
     # spec = "F[0,3](x < -1)"  # TODO: Start from 3
-    spec = "G[0,5.5](F[0,4](x < 0.4))"
+    # spec = "G[0,5.5](F[0,4](x < 0.4))"
 
     # spec = "F[3,10](G[0,5](x > 7)) & G[0,8](y < 4 & z>2)"
     # spec = "((x < 1) | (y>2) ) & x<3"
 
-    # Parses STL formula into python objects
-    STL = STLParser(spec)
-    print(STL.spec)
+    # M = 1000000000
+    # et = 0.000000001
+    M = 1234
+    et = 0.1
 
+    # Parses STL formula into python objects
+    spec = STLParser(spec).spec
+    print(spec)
+
+    opt = gp.Model()
+
+    used_predicates = {}
+    info = {"M": M, "et": et, "dt": DT, "aux_count": 0, "const_count": 0}
+    v = spec.create_aux(opt, 1, info, used_predicates)
+    opt.update()
+    print(v)
+
+    opt.write("test.lp")
 
     # Visualization of the signal
-    rng = STL.range(0, SIGNAL_END, STEP)
-    plt.plot(rng, signal(rng))
-    plt.show()
+    # rng = STL.range(0, SIGNAL_END, STEP)
+    # plt.plot(rng, signal(rng))
+    # plt.show()
 
     # Printing the robustness degree
-    print(STL.r(signal, STL.spec, SIGNAL_START))
+    # print(STL.r(signal, STL.spec, SIGNAL_START))
